daa58dab52b4ab64cf8db92fca2cd625d97848e7
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2020-09-07 01:36:21 +0530

    Update Crimson Winding Staircase.py

--- Crimson Winding Staircase.py
+++ Crimson Winding Staircase.py
@@ -1,5 +1,5 @@
 from turtle import *
-speed(0)           # fastest cursor speed of out Python Turtle
+speed(0)                  # fastest cursor speed of out Python Turtle
 
 x = 1
 
@@ -7,7 +7,7 @@ while x < 200:
     r = 175
     g = 0
     b = 42
-    pencolor(r,g,b)
+    pencolor(r,g,b)      # Alabama Crimson RGB color selected from https://rgbcolorcode.com/color/alabama-crimson
     fd(50 + x)
     rt(90.911)
     x = x+1
