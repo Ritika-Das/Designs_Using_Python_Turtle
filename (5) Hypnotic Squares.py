a902cc91969ab4e194504721ee63e6c4c58bf004
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2020-09-07 06:57:48 +0530

    Update (5) Hypnotic Squares.py

--- (5) Hypnotic Squares.py
+++ (5) Hypnotic Squares.py
@@ -1,5 +1,5 @@
 from turtle import *
-speed(4)    # comment this line if you want to see the turtle in action, but slower
+speed(10)    # comment this line if you want to see the turtle in action, but slower
 pencolor(128,0,128)    # RGB coordinates of the color 'purple'
 fd(175)
 lt(90)
